c816996d3eec0dd95f58edd5e5bce16f9489e6d9
Ritik Gupta <61592876+ritik955@users.noreply.github.com>
2022-08-25 08:42:37 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div id="main">
+    <h1 data-ns-test="project-name">Dream Project</h1>
+    <p data-ns-test="project-description">Project Description</p>
+    </div>
+  );
+}
+
+
+export default App;
